a93b8e656d44ad3342925e700478a5c49b2fb460
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-04-01 14:21:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
 });
 
 app.post('/api.create', (req, res) => {
+    const username = req.body.userName
+    const title = req.body.title
+    const text = req.body.text
+    
+    
+
     db.query("INSERT INTO posts (title, post_text, username) VALUES ()", (err, result) => {
         if (err) {
             console.log(err)
